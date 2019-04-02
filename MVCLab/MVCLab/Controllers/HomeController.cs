af27035d01f13ca5d11e2d812bace9d648ac5a8d
daniel.logerstedt@gmail.com <daniel.logerstedt@gmail.com>
2019-04-02 14:39:44 -0700

    Built MVC with Routing

---
+++ MVCLab/MVCLab/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCLab.Models;
+
+namespace MVCLab.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int priceMax, int pointMin)
+        {
+            return RedirectToAction("Results", new { priceMax, pointMin } );
+        }
+
+        [HttpGet]
+        public IActionResult Results(int priceMax, int pointMin)
+        {
+            List<Wine> wines = Wine.GetWineList(priceMax, pointMin);
+            return View(wines);
+        }
+
+
+
+
+    }
+
+}
---
+++ MVCLab/MVCLab/Models/Wine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MVCLab.Models
+{
+    public class Wine
+    {
+        public int ID { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public string Designation { get; set; }
+        public int Points { get; set; }
+        public decimal Price { get; set; }
+        public string Region_1 { get; set; }
+        public string Region_2 { get; set; }
+        public string Variety { get; set; }
+        public string Winery { get; set; }
+
+        public static List<Wine> GetWineList(int price, int points)
+        {
+            var reader = new StreamReader(File.OpenRead("../wine.csv"));
+            List<Wine> wines = new List<Wine>();
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+
+                // This Regex came from stack overflow: https://stackoverflow.com/questions/6542996/how-to-split-csv-whose-columns-may-contain
+                string[] data = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                wines.Add(new Wine()
+                {
+                    ID = Convert.ToInt32(data[0]),
+                    Country = data[1],
+                    Description = data[2],
+                    Designation = data[3],
+                    Points = int.TryParse(data[4], out int point) ? point : -1,
+                    Price = Decimal.TryParse(data[5], out Decimal cost) ? cost : Decimal.MaxValue,
+                    Region_1 = data[6],
+                    Region_2 = data[7],
+                    Variety = data[8],
+                    Winery = data[9]    
+                });
+
+            }
+            
+            List<Wine> wineList = wines.Where(wine => (wine.Points >= points && wine.Price <= price)).ToList();
+            return wineList;
+        }
+       
+    }
+}
